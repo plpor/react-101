260275f24cca421fadfd592d54599451284c4928
plpor <pl.por@hotmail.com>
2022-09-17 14:08:24 +0700

    version 2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
 		<h1>Hello React App</h1>
+		<hr/>
+		<h2>ได้รับการแก้ไขเป็น version : 2.0</h2>
       </header>
     </div>
   );
