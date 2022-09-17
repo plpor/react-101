94afd34691350ee71b1924c61be61399a0e6cfc0
plpor <pl.por@hotmail.com>
2022-09-17 14:21:31 +0700

    version 3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
 		<h1>Hello React App</h1>
 		<hr/>
 		<h2>ได้รับการแก้ไขเป็น version : 2.0</h2>
+		<hr/>
+		<h2>ได้รับการแก้ไขเป็น version : 3.0 (auto deploy)</h2>
       </header>
     </div>
   );
